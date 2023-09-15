bc25acf19c809efdec04b39686e7355927e72130
Cyber MinOut <131236146+cybermindout@users.noreply.github.com>
2023-09-15 17:34:49 +0530

    updates

--- Brototype/Pattern/tempCodeRunnerFile.c
+++ Brototype/Pattern/tempCodeRunnerFile.c
@@ -1,24 +1,18 @@
-#include<stdio.h>
-int main() 
-{
-    int i,j,k;
-    for(i=1;i<=6;i++)
-    {
-      k=1;
-      for(j=1;j<=11;j++)
-      {
-        if(j==7-i || j==5+i)
-        {    
-      printf("*");
-        }
-      else if(j%2!=0 && i==6)
-      {
-        printf("*");
-      }
-      else
-      printf(" ");
+#include<stdio.h.>
+int main(){
+   int i,j,k;
+   for(i=1;i<=4;i++){
+      for(j=1;j<=(i*2)-1;j++){
+         printf("%c",i+64);
 
-    }
-    printf("\n");
-    }
- }
+      }
+      printf("\n");
+      for(k=1;k<=i*2;k++){
+         if(i==4)
+         break;
+         printf("%d",i);
+
+      }
+      printf("\n");
+   }
+}
